bce4c4e2fb0845d2a1eb964f67b37348e2462748
Bobby T <bob.tarino@gmail.com>
2021-08-13 14:35:40 -0500

    passed schema data in startApollloServer function

--- server/server.js
+++ server/server.js
@@ -1,43 +1,49 @@
 const express = require('express');
+const path = require('path');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
-
+const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
-const app = express();
+
+
+
 // create a new Apollo server and pass in our schema data
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
 
-// integrate our Apollo server with the Express application as middleware
-server.applyMiddleware({ app });
+  const app = express();
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-// Serve up static assets // only comes into effect when in production
-// if in production - instruct the Express.js server to serve any files in the React application's build directory in the client folder
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  // Serve up static assets // only comes into effect when in production
+  // if in production - instruct the Express.js server to serve any files in the React application's build directory in the client folder
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  // wildcard GET route 
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      // log where we can go to test our GQL API
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
 }
 
-// wildcard GET route 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
+startApolloServer();
