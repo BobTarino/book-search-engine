9721fb2638d6c374bfcc7840394d74385ac32f2f
Bobby T <bob.tarino@gmail.com>
2021-08-13 11:16:22 -0500

    established link to GraphQL and instantiated Apollo Client instance

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
+// establish a link to the GraphQL server at its /graphql endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// use the setContext() function to retrieve the token from localStorage and set the HTTP request headers of every request to include the token
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+
+// use the ApolloClient() constructor to instantiate the Apollo Client instance , create the connection to the API endpoint, and instantiate a new cache object for memory
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
